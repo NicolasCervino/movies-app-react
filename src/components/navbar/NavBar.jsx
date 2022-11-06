5f418ef5d42052d2c1065d5dd360e994232ba979
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-06 20:46:33 -0300

    se cambio el background del navbar

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,7 +4,7 @@ import "./style/navbar.css";
 
 const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="transparent" variant="dark" expand="lg">
             <Container>
                 <Link to="/home" className="navbar-brand logo">
                     MoviesApp
