95c164127dae37d81cb12e3593178297343740e0
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-06 20:48:21 -0300

    FeaturedMovies obtiene las peliculas populares y crea un carrousel

---
+++ src/components/featuredMovies/FeaturedMovies.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+import Api from "../../service/api";
+import MainCarrousel from "../mainCarrousel/MainCarrousel";
+
+const FeaturedMovies = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        Api.getPopularMovies()
+            .then((data) => {
+                setMovies(data.results);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return <MainCarrousel movies={movies} size={5}></MainCarrousel>;
+};
+
+export default FeaturedMovies;
