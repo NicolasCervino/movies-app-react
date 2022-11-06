7dca431d67a52c4f197945ba828d3d20c27c08a1
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-06 20:45:08 -0300

    se agrega un archivo con la logica para hacer peticiones a la api

---
+++ src/service/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const apiKey = "f3b242b5857fe6135b2f4c0420e0ba0b";
+
+const getPopularMovies = async () => {
+    // Obtiene las peliculas populares de la base de datos
+    return await axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ARG`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => console.log(error));
+};
+
+const Api = {
+    getPopularMovies,
+};
+
+export default Api;
