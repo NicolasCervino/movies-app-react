63fe52be8a463421bc12980aa25aa277111eb3f0
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-06 21:52:01 -0300

    se arreglaron las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/home/Home";
+import Home from "./routes/home/Home";
 import "./App.css";
 import "./assets/scss/main.css";
-import Navbar from "./components/navbar/NavBar";
-import FeaturedMovies from "./components/featuredMovies/FeaturedMovies";
+import Layout from "./layout/Layout";
+import MyList from "./routes/myList/MyList";
+import Movies from "./routes/movies/Movies";
+import NotFound from "./routes/404/NotFound";
 
 function App() {
     return (
         <div className="App">
-            <header className="container-fluid">
-                <Navbar></Navbar>
-            </header>
-            <main className="container-fluid p-0">
-                <FeaturedMovies></FeaturedMovies>
-                <Routes>
-                    <Route path="/" element={<Navigate to="/home" />} />
-                    <Route path="/home" element={<Home></Home>} />
-                    <Route path="/movie/:id" element={<h1>Hello World</h1>} />
-                    <Route path="/search" element={<h1>Hello World</h1>} />
-                    <Route path="/genres" element={<h1>Hello World</h1>} />
-                    <Route path="/genre/:genreName" element={<h1>Hello World</h1>} />
-                    <Route path="*" element={<h1>Not found</h1>} />
-                </Routes>
-            </main>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Navigate replace to="home" />} />
+                    <Route path="home" element={<Home />} />
+                    <Route path="my-list" element={<MyList />} />
+                    <Route path="movies" element={<Movies />} />
+                    <Route path="movie/:id" element={<h1>Hello World</h1>} />
+                    <Route path="tv-shows" element={<h1>Tv Shows</h1>} />
+                    <Route path="search" element={<h1>Hello World</h1>} />
+                    <Route path="genres" element={<h1>Hello World</h1>} />
+                    <Route path="genre/:genreName" element={<h1>Hello World</h1>} />
+                </Route>
+                <Route path="*" element={<Navigate replace to="/error" />} />
+                <Route path="error" element={<NotFound></NotFound>} />
+            </Routes>
         </div>
     );
 }
