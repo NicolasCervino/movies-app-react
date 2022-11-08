f93c37110ae90df6bd61a5e6e3dc27eee8e2c203
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-07 21:44:47 -0300

    se agrega una funcion para obtener series populares

--- src/service/api.js
+++ src/service/api.js
@@ -12,8 +12,19 @@ const getPopularMovies = async () => {
         .catch((error) => console.log(error));
 };
 
+const getPopularShows = async () => {
+    // Obtiene las peliculas populares de la base de datos
+    return await axios
+        .get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=es-ARG`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => console.log(error));
+};
+
 const Api = {
     getPopularMovies,
+    getPopularShows,
 };
 
 export default Api;
