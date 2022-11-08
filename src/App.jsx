af55ba6052d6cb4b1f97ca802d3ebd52594a4ca0
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-07 21:41:57 -0300

    se agrega el provider en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { MyListProvider } from "./context/ListContext";
 import Home from "./routes/home/Home";
 import "./App.css";
 import "./assets/scss/main.css";
@@ -10,21 +11,23 @@ import NotFound from "./routes/404/NotFound";
 function App() {
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Navigate replace to="home" />} />
-                    <Route path="home" element={<Home />} />
-                    <Route path="my-list" element={<MyList />} />
-                    <Route path="movies" element={<Movies />} />
-                    <Route path="movie/:id" element={<h1>Hello World</h1>} />
-                    <Route path="tv-shows" element={<h1>Tv Shows</h1>} />
-                    <Route path="search" element={<h1>Hello World</h1>} />
-                    <Route path="genres" element={<h1>Hello World</h1>} />
-                    <Route path="genre/:genreName" element={<h1>Hello World</h1>} />
-                </Route>
-                <Route path="*" element={<Navigate replace to="/error" />} />
-                <Route path="error" element={<NotFound></NotFound>} />
-            </Routes>
+            <MyListProvider>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Navigate replace to="home" />} />
+                        <Route path="home" element={<Home />} />
+                        <Route path="my-list" element={<MyList />} />
+                        <Route path="movies" element={<Movies />} />
+                        <Route path="movie/:id" element={<h1>Hello World</h1>} />
+                        <Route path="tv-shows" element={<h1>Tv Shows</h1>} />
+                        <Route path="search" element={<h1>Hello World</h1>} />
+                        <Route path="genres" element={<h1>Hello World</h1>} />
+                        <Route path="genre/:genreName" element={<h1>Hello World</h1>} />
+                    </Route>
+                    <Route path="*" element={<Navigate replace to="/error" />} />
+                    <Route path="error" element={<NotFound></NotFound>} />
+                </Routes>
+            </MyListProvider>
         </div>
     );
 }
