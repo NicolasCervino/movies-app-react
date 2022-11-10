9323c3f9bdd4146eacc86d237677c1a21d4674e5
Nicolas Cervino <nicolascervino2@gmail.com>
2022-11-10 00:32:21 -0300

    se agrego un metodo para obtener detalles de una pelicula

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const apiKey = "f3b242b5857fe6135b2f4c0420e0ba0b";
 
 const getPopularMovies = async () => {
-    // Obtiene las peliculas populares de la base de datos
     return await axios
         .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ARG`)
         .then((response) => {
@@ -13,7 +12,6 @@ const getPopularMovies = async () => {
 };
 
 const getPopularShows = async () => {
-    // Obtiene las peliculas populares de la base de datos
     return await axios
         .get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=es-ARG`)
         .then((response) => {
@@ -22,9 +20,16 @@ const getPopularShows = async () => {
         .catch((error) => console.log(error));
 };
 
+const getMovieData = async (movieId) => {
+    return await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es-ARG`).then((response) => {
+        return response;
+    });
+};
+
 const Api = {
     getPopularMovies,
     getPopularShows,
+    getMovieData,
 };
 
 export default Api;
