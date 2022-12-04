ae47635c3f8a2ac78cd3a6d3f8f3deea17946424
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-04 05:12:44 -0300

    CarrouselItem utiliza un state para cambiar al estilo mobile o desktop

--- src/components/carrousel/carrouselItem/CarrouselItem.jsx
+++ src/components/carrousel/carrouselItem/CarrouselItem.jsx
@@ -1,24 +1,43 @@
 import "./carrousel-item.css";
+import { useState, useEffect } from "react";
 import CarrouselItemButtons from "../carrouselItemButtons/CarrouselItemsButtons";
 
 const CarrouselItem = ({ movie, type }) => {
-    return (
-        <>
-            <div className="img-overlay">
-                {/* Imagen Principal */}
-                <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} className="d-none d-sm-block w-100" />
-                {/* Imagen Mobile */}
-                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="d-block d-sm-none w-100" />
-            </div>
+    const [isMobile, setIsMobile] = useState(false);
 
-            <div className="carousel-caption container">
-                <div className="row w-50">
-                    <h2 className="text-start titulo-carrousel">{type === "movies" ? movie.title : movie.name}</h2>
-                    <p className="text-start subtitulo-carrousel">{movie.overview.slice(0, 200) + "..."}</p>
-                </div>
+    const handleResize = () => {
+        if (window.innerWidth < 575) {
+            setIsMobile(true);
+        } else {
+            setIsMobile(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    });
+
+    return (
+        <div
+            className="main-slide"
+            style={
+                isMobile
+                    ? { backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.poster_path})` }
+                    : { backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})` }
+            }
+        >
+            <div className={`carousel-caption text-start ${!isMobile ? "w-50 px-2" : ""}`}>
+                <h2 className="titulo-carrousel">{type === "movies" ? movie.title : movie.name}</h2>
+                <p className="subtitulo-carrousel">
+                    {!isMobile ? movie.overview.slice(0, 300) + "..." : movie.overview.slice(0, 150) + "..."}
+                </p>
                 <CarrouselItemButtons movie={movie} type={type} />
             </div>
-        </>
+        </div>
     );
 };
 
