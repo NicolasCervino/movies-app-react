d655a7a53968d656b957ba139dd52b57e012ddcb
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-05 04:12:54 -0300

    Home muestra algunas cards de prueba

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,3 +1,25 @@
-const Home = () => {};
+import { useState, useEffect } from "react";
+import Api from "../../service/api";
+import MovieCard from "../../components/movieCard/MovieCard";
+
+const Home = () => {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        Api.getPopularMovies()
+            .then((data) => {
+                setMovies(data.results);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+    return (
+        <div className="row px-2">
+            {movies.map((m) => (
+                <MovieCard key={m.id} movie={m} type={"movies"} />
+            ))}
+        </div>
+    );
+};
 
 export default Home;
