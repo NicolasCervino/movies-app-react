d3920e274bae153415c13ae1cd1f03dfa4386696
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-08 19:25:16 -0300

    se agrega lazy loading a las imagenes

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -7,7 +7,12 @@ const MovieCard = ({ element, type }) => {
         <>
             <div className={"card movie-card position-relative"}>
                 <Link to={type === "movies" ? `/movie/${element.id}` : `/tv-show/${element.id}`}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`} className="card-img-top" draggable="false" />
+                    <img
+                        src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`}
+                        loading={"lazy"}
+                        className="card-img-top"
+                        draggable={false}
+                    />
                 </Link>
                 <MovieCardButton element={element} type={type} />
             </div>
