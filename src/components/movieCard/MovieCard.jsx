42c45d8cf095df4e14f5c19f7caf0d888f1ec947
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-08 12:45:37 -0300

    la imagen de cada card es ahora un link

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ element, type }) => {
     return (
-        <div className="col-6 col-sm-3 col-xl-2 p-2">
-            <div className={"card position-relative"}>
-                <img src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`} className="card-img-top" draggable="false" />
+        <>
+            <div className={"card movie-card position-relative"}>
+                <Link to={type === "movies" ? `/movie/${element.id}` : `/tv-show/${element.id}`}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${element.poster_path}`} className="card-img-top" draggable="false" />
+                </Link>
                 <MovieCardButton element={element} type={type} />
             </div>
 
             <Link className="titulo-card text-white" to={type === "movies" ? `/movie/${element.id}` : `/tv-show/${element.id}`}>
-                {element.title}
+                {type === "movies" ? element.title : element.name}
             </Link>
-        </div>
+        </>
     );
 };
 
