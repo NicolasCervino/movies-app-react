5acba6db01c2d41f10d7c774b4a288fa5f061586
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-12 15:46:40 -0300

    se agrega un metodo para obtener info de proximas peliculas

--- src/service/api.js
+++ src/service/api.js
@@ -4,7 +4,7 @@ const apiKey = "f3b242b5857fe6135b2f4c0420e0ba0b";
 
 const getPopularMovies = async () => {
     return await axios
-        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ARG`)
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-AR`)
         .then((response) => {
             return response.data;
         })
@@ -13,16 +13,16 @@ const getPopularMovies = async () => {
 
 const getPopularShows = async () => {
     return await axios
-        .get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=es-ARG`)
+        .get(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=es-AR`)
         .then((response) => {
             return response.data;
         })
         .catch((error) => console.log(error));
 };
 
-const getMovieData = async (movieId) => {
+const getData = async (movieId, type) => {
     return await axios
-        .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=es-ARG&append_to_response=credits`)
+        .get(`https://api.themoviedb.org/3/${type}/${movieId}?api_key=${apiKey}&language=es-AR&append_to_response=credits`)
         .then((response) => {
             return response;
         });
@@ -46,12 +46,22 @@ const getTopShows = async () => {
         .catch((error) => console.log(error));
 };
 
+const getUpcomingMovies = async () => {
+    return await axios
+        .get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=es-AR`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => console.log(error));
+};
+
 const Api = {
     getPopularMovies,
     getPopularShows,
-    getMovieData,
+    getData,
     getTopMovies,
     getTopShows,
+    getUpcomingMovies,
 };
 
 export default Api;
