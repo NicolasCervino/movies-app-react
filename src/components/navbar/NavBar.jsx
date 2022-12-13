9da4bf004a409d4b837e374313d4ced5aab0cfad
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-13 00:21:51 -0300

    Navbar cambia el background al hacer scroll

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./style/navbar.css";
 
 const NavBar = () => {
+    const [background, setBackground] = useState("transparent");
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY >= 150) {
+            setBackground("dark");
+        } else {
+            setBackground("transparent");
+        }
+    };
+
     return (
-        <Navbar bg="transparent" variant="dark" expand="lg">
+        <Navbar bg={background} variant="dark" expand="lg" onToggle={() => setBackground("dark")}>
             <Container>
                 <Link to="/home" className="navbar-brand logo">
                     MoviesApp
