ab830213ef4b2aaf3d2820c67fac024270c2002c
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-12 21:50:43 -0300

    FeaturedShows muestra las mejor valoradas

--- src/components/featuredShows/FeaturedShows.jsx
+++ src/components/featuredShows/FeaturedShows.jsx
@@ -6,7 +6,7 @@ const FeaturedShows = () => {
     const [shows, setShows] = useState([]);
 
     useEffect(() => {
-        Api.getPopular("tv")
+        Api.getTop("tv")
             .then((data) => {
                 const shows = data.results.filter((s) => s.backdrop_path !== null && s.overview !== "");
                 setShows(shows);
