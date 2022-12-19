7d5797f459fc5c012c7f21dbab45caa8f000c8f1
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-19 18:58:00 -0300

    InfiniteCardScroll renderiza cards para mas peliculas segun la categoria dada

---
+++ src/components/InfiniteScroll/InfiniteCardScroll.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Api from "../../service/api";
+import MovieCard from "../movieCard/MovieCard";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+function InfiniteCardScroll({ elements, setElements, type, category, totalPages }) {
+    const [page, setpage] = useState(2);
+
+    const [hasMore, sethasMore] = useState(true);
+
+    const fetchData = async () => {
+        if (page <= totalPages) {
+            Api.getByCategory(category, type, page).then((data) => {
+                setpage(page + 1);
+                setElements(elements.concat(data.results));
+            });
+        } else {
+            sethasMore(false);
+        }
+    };
+
+    return (
+        <InfiniteScroll
+            dataLength={elements.length}
+            next={fetchData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <p className="text-white" style={{ textAlign: "center" }}>
+                    <b>Yay! You have seen it all</b>
+                </p>
+            }
+            className="row"
+        >
+            {elements.map((e) => (
+                <div className="col-6 col-sm-3 col-xl-2 p-2" key={e.id}>
+                    <MovieCard element={e} type={"movies"} />
+                </div>
+            ))}
+        </InfiniteScroll>
+    );
+}
+
+export default InfiniteCardScroll;
