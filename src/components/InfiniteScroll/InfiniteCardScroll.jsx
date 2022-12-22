56db7ace6d2642c857971e361ee0713acb4e449a
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-22 19:06:54 -0300

    InfiniteCardScroll tambien funciona para peliculas de un genero

--- src/components/InfiniteScroll/InfiniteCardScroll.jsx
+++ src/components/InfiniteScroll/InfiniteCardScroll.jsx
@@ -3,14 +3,19 @@ import Api from "../../service/api";
 import MovieCard from "../movieCard/MovieCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-function InfiniteCardScroll({ elements, setElements, type, category, totalPages }) {
+function InfiniteCardScroll({ elements, setElements, type, category, genreId, totalPages }) {
     const [page, setpage] = useState(2);
 
     const [hasMore, sethasMore] = useState(true);
 
     const fetchData = async () => {
-        if (page <= totalPages) {
+        if (page <= totalPages && page <= 500 && category) {
             Api.getByCategory(category, type, page).then((data) => {
+                setpage(page + 1);
+                setElements(elements.concat(data.results));
+            });
+        } else if (page <= totalPages && page <= 500 && genreId) {
+            Api.getByGenre(type, genreId, page).then((data) => {
                 setpage(page + 1);
                 setElements(elements.concat(data.results));
             });
@@ -34,7 +39,7 @@ function InfiniteCardScroll({ elements, setElements, type, category, totalPages 
         >
             {elements.map((e) => (
                 <div className="col-6 col-sm-3 col-xl-2 p-2" key={e.id}>
-                    <MovieCard element={e} type={"movies"} />
+                    <MovieCard element={e} type={type} />
                 </div>
             ))}
         </InfiniteScroll>
