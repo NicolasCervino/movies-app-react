d42d243393bde1c416254891afe48a21aaa4fb0b
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-21 21:29:21 -0300

    FeaturedMovies muestra un carrousel vacio mientras esta cargando

--- src/components/featuredMovies/FeaturedMovies.jsx
+++ src/components/featuredMovies/FeaturedMovies.jsx
@@ -1,19 +1,23 @@
 import { useState, useEffect } from "react";
 import Api from "../../service/api";
+import LoadingCarrousel from "../loaders/loadingCarrousel/LoadingCarrousel";
 import MainCarrousel from "../carrousel/mainCarrousel/MainCarrousel";
 
 const FeaturedMovies = () => {
     const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         Api.getPopular("movie")
             .then((data) => {
                 setMovies(data.results);
             })
-            .catch((error) => console.log(error));
+            .catch((error) => console.log(error))
+            .finally(setTimeout(() => setLoading(false), 1000));
     }, []);
 
-    return <MainCarrousel movies={movies} type={"movies"} size={5}></MainCarrousel>;
+    return loading ? <LoadingCarrousel /> : <MainCarrousel movies={movies} type={"movies"} size={5} />;
 };
 
 export default FeaturedMovies;
