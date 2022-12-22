7fa208f28a06528b3e79879325b671b4138a3111
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-22 19:07:41 -0300

    CategorySelector tambien funciona para generos

--- src/components/categorySelector/CategorySelector.jsx
+++ src/components/categorySelector/CategorySelector.jsx
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./category-selector.css";
 
-const CategorySelector = ({ options, category, setCategory }) => {
+const CategorySelector = ({ options, category, setCategory, genre }) => {
+    const navigate = useNavigate();
+
     // Change category and hash on select change
     const handleSelectChange = (e) => {
-        setCategory(e.target.value);
-        window.location.hash = "#" + e.target.value;
+        if (category) {
+            setCategory(e.target.value);
+            window.location.hash = "#" + e.target.value;
+        } else if (genre) {
+            navigate(`/genre/${genre[0]}/${e.target.value}`);
+        }
     };
 
     return (
         <div className="category-select">
-            <select onChange={(e) => handleSelectChange(e)} defaultValue={category}>
+            <select onChange={(e) => handleSelectChange(e)} defaultValue={category || genre[1]}>
                 {options.map((o) => (
                     <option key={o.value} value={o.value}>
                         {o.label}
