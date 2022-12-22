cd6e98255bc969500313fd28f4d951cf067b3a0f
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-21 21:36:18 -0300

    FeaturedShows incluye un estado de carga

--- src/components/featuredShows/FeaturedShows.jsx
+++ src/components/featuredShows/FeaturedShows.jsx
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
 import Api from "../../service/api";
+import LoadingCarrousel from "../loaders/loadingCarrousel/LoadingCarrousel";
 import MainCarrousel from "../carrousel/mainCarrousel/MainCarrousel";
 
 const FeaturedShows = () => {
     const [shows, setShows] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
         Api.getTop("tv")
             .then((data) => {
                 const shows = data.results.filter((s) => s.backdrop_path !== null && s.overview !== "");
                 setShows(shows);
             })
-            .catch((error) => console.log(error));
+            .catch((error) => console.log(error))
+            .finally(setTimeout(() => setLoading(false), 1000));
     }, []);
 
-    return <MainCarrousel movies={shows} type={"shows"} size={5}></MainCarrousel>;
+    return loading ? <LoadingCarrousel /> : <MainCarrousel movies={shows} type={"shows"} size={5} />;
 };
 
 export default FeaturedShows;
