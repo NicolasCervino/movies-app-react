f0ff5ee4a58339ca554eab2e108b839993138132
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-26 16:40:24 -0300

    se refactorizo el metodo fetchData

--- src/components/InfiniteScroll/InfiniteCardScroll.jsx
+++ src/components/InfiniteScroll/InfiniteCardScroll.jsx
@@ -2,24 +2,35 @@ import { useState } from "react";
 import Api from "../../service/api";
 import MovieCard from "../movieCard/MovieCard";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { useLocation } from "react-router-dom";
 
-function InfiniteCardScroll({ elements, setElements, type, category, genreId, totalPages }) {
+function InfiniteCardScroll({ elements, setElements, type, totalPages }) {
     const [page, setpage] = useState(2);
-
     const [hasMore, sethasMore] = useState(true);
 
+    // Posible types
+    // type = ["movie/tv", "category/genre/search", category/genreId,query]
     const fetchData = async () => {
-        if (page <= totalPages && page <= 500 && category) {
-            Api.getByCategory(category, type, page).then((data) => {
-                setpage(page + 1);
-                setElements(elements.concat(data.results));
-            });
-        } else if (page <= totalPages && page <= 500 && genreId) {
-            Api.getByGenre(type, genreId, page).then((data) => {
-                setpage(page + 1);
-                setElements(elements.concat(data.results));
-            });
+        if (page <= totalPages && page <= 500) {
+            switch (type[1]) {
+                case "category":
+                    Api.getByCategory(type[2], type[0], page).then((data) => {
+                        setpage(page + 1);
+                        setElements(elements.concat(data.results));
+                    });
+                    break;
+                case "genre":
+                    Api.getByGenre(type[0], type[2], page).then((data) => {
+                        setpage(page + 1);
+                        setElements(elements.concat(data.results));
+                    });
+                    break;
+                case "search":
+                    Api.getSearch(type[0], type[2], page).then((data) => {
+                        setpage(page + 1);
+                        setElements(elements.concat(data.results));
+                    });
+                    break;
+            }
         } else if (page > totalPages || page > 500) {
             sethasMore(false);
         }
@@ -40,7 +51,7 @@ function InfiniteCardScroll({ elements, setElements, type, category, genreId, to
         >
             {elements.map((e) => (
                 <div className="col-6 col-sm-3 col-xl-2 p-2" key={e.id}>
-                    <MovieCard element={e} type={type} />
+                    <MovieCard element={e} type={type[0]} />
                 </div>
             ))}
         </InfiniteScroll>
