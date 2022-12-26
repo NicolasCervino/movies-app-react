29548f6ce363a2339f17753232f29e38960d39e3
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-26 19:42:18 -0300

    se elimino codigo innecesario y algunos cambios de estilo

--- src/routes/search/Search.jsx
+++ src/routes/search/Search.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import Api from "../../service/api";
-
 import InfiniteCardScroll from "../../components/InfiniteScroll/InfiniteCardScroll";
 
 const Search = () => {
@@ -9,41 +8,26 @@ const Search = () => {
     const [query, setQuery] = useState(searchParams.get("q"));
     const [elements, setElements] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
-    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         setQuery(searchParams.get("q"));
-        setLoading(true);
         if (query !== null) {
-            Api.getSearch("movie", query)
-                .then((data) => {
-                    setTotalPages(data.total_pages);
-                    setElements(data.results);
-                })
-                .finally(setLoading(false));
+            Api.getSearch("movie", query).then((data) => {
+                setTotalPages(data.total_pages);
+                setElements(data.results);
+            });
         }
     }, [searchParams.get("q"), query]);
 
-    // const fetchData = async () => {
-    //     if (page <= totalPages && page <= 500) {
-    //         Api.getSearch("movie", query, page).then((data) => {
-    //             setpage(page + 1);
-    //             setElements(elements.concat(data.results));
-    //         });
-    //     } else if (page > totalPages || page > 500) {
-    //         sethasMore(false);
-    //     }
-    // };
-
     return (
-        <div>
-            {!query ? (
-                <h1 className="text-white">No hay resultados de busqueda</h1>
+        <div className="row">
+            {!query || elements.length === 0 ? (
+                <h1 className="text-white">No hay resultados...</h1>
             ) : (
                 <>
                     <h1 className="text-white">
                         Resultados para:
-                        <span> {decodeURI(query)}</span>
+                        <span style={{ color: "var(--main-color)" }}> {decodeURI(query)}</span>
                     </h1>
                     <div className="row">
                         <InfiniteCardScroll
