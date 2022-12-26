3c30fdbeb1d4e107c1e1a688947e776b381ecf76
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-26 16:41:47 -0300

    searchBar navega a /search al pulsar la tecla enter

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,21 @@
 import { useState } from "react";
 import "./search-bar.css";
+import { useNavigate, createSearchParams } from "react-router-dom";
 
 const SearchBar = () => {
     const [active, setActive] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleEnter = (e) => {
+        if (e.key === "Enter" && searchQuery.trim() !== "") {
+            navigate({
+                pathname: "search",
+                search: `?${createSearchParams({ q: encodeURI(searchQuery) })}`,
+            });
+        }
+    };
 
     const handleInputChange = (e) => {
         setSearchQuery(e.target.value);
@@ -17,7 +29,14 @@ const SearchBar = () => {
         <div className={active ? "search active" : "search"}>
             <div className="icon" onClick={() => setActive(!active)}></div>
             <div className={active ? "input" : "d-none"}>
-                <input type="text" spellCheck={false} placeholder="Busqueda" value={searchQuery} onChange={handleInputChange} />
+                <input
+                    type="text"
+                    spellCheck={false}
+                    placeholder="Busqueda"
+                    value={searchQuery}
+                    onKeyDown={handleEnter}
+                    onChange={handleInputChange}
+                />
             </div>
             <span className={active ? "clear" : "d-none"} onClick={clearText}></span>
         </div>
