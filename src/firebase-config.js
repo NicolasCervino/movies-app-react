1a68c2049d7b84db83a89d95c9e2a97065cf107f
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-26 22:11:04 -0300

    se agrega firebase al proyecto

---
+++ src/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDpYYRQ1WvUQVIvhEzsh3yR_SsMJ3HIbZA",
+    authDomain: "moviesapp-f0996.firebaseapp.com",
+    projectId: "moviesapp-f0996",
+    storageBucket: "moviesapp-f0996.appspot.com",
+    messagingSenderId: "97789016613",
+    appId: "1:97789016613:web:cca1a60836b70ebff6ddcc",
+    measurementId: "G-5F8DWTH5YB",
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import * as bootstrap from "bootstrap";
 import { BrowserRouter } from "react-router-dom";
+import { FirebaseAppProvider } from "reactfire";
+import firebaseConfig from "./firebase-config";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+            <Suspense>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Suspense>
+        </FirebaseAppProvider>
     </React.StrictMode>
 );
