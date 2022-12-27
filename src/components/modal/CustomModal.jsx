824c134fca38430b5113f445393ec39eb68fe7db
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-26 22:12:27 -0300

    custom modal ahora puede variar su tamaño

--- src/components/modal/CustomModal.jsx
+++ src/components/modal/CustomModal.jsx
@@ -1,9 +1,9 @@
 import { Modal } from "react-bootstrap";
 import "./custom-modal.css";
 
-const CustomModal = ({ show, handleClose, children }) => {
+const CustomModal = ({ show, handleClose, children, size }) => {
     return (
-        <Modal show={show} onHide={handleClose} centered={true} size={"lg"}>
+        <Modal show={show} onHide={handleClose} centered={true} size={size} contentClassName="custom-modal-content">
             <Modal.Body className="p-0 bg-dark text-white">{children}</Modal.Body>
         </Modal>
     );
