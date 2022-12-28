435b53855d4623a03454dbe8358923e2c2321246
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-27 22:45:00 -0300

    se agrega AuthProvider y rutas para login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { MyListProvider } from "./context/ListContext";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./routes/home/Home";
 import "./App.css";
 import "./assets/scss/main.css";
@@ -13,6 +14,8 @@ import Shows from "./routes/shows/Shows";
 import Genres from "./routes/genres/Genres";
 import GenreInfo from "./routes/genreInfo/GenreInfo";
 import Search from "./routes/search/Search";
+import Login from "./routes/login/Login";
+import Register from "./routes/register/Register";
 
 function App() {
     const location = useLocation();
@@ -20,35 +23,42 @@ function App() {
 
     return (
         <div className="App">
-            <MyListProvider>
-                <Routes location={background || location}>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Navigate replace to="home" />} />
-                        <Route path="home" element={<Home />} />
-                        <Route path="my-list" element={<MyList />} />
+            <AuthProvider>
+                <MyListProvider>
+                    <Routes location={background || location}>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Navigate replace to="home" />} />
+                            <Route path="home" element={<Home />}>
+                                <Route path="login" element={<Login />} />
+                                <Route path="register" element={<Register />} />
+                            </Route>
+                            <Route path="my-list" element={<MyList />} />
 
-                        <Route path="movies" element={<Movies />}>
-                            <Route path=":id" element={<MovieInfo />} />
+                            <Route path="movies" element={<Movies />}>
+                                <Route path=":id" element={<MovieInfo />} />
+                            </Route>
+
+                            <Route path="tv-shows" element={<Shows />}>
+                                <Route path=":id" element={<ShowInfo />} />
+                            </Route>
+
+                            <Route path="search" element={<Search />} />
+                            <Route path="genres" element={<Genres />} />
+                            <Route path="genre/:type/:genreName" element={<GenreInfo />} />
                         </Route>
-
-                        <Route path="tv-shows" element={<Shows />}>
-                            <Route path=":id" element={<ShowInfo />} />
-                        </Route>
-
-                        <Route path="search" element={<Search />} />
-                        <Route path="genres" element={<Genres />} />
-                        <Route path="genre/:type/:genreName" element={<GenreInfo />} />
-                    </Route>
-                    <Route path="*" element={<Navigate replace to="/error" />} />
-                    <Route path="error" element={<NotFound></NotFound>} />
-                </Routes>
-                {background && (
-                    <Routes>
-                        <Route path="movies/:id" element={<MovieInfo />} />
-                        <Route path="tv-shows/:id" element={<ShowInfo />} />
+                        <Route path="*" element={<Navigate replace to="/error" />} />
+                        <Route path="error" element={<NotFound></NotFound>} />
                     </Routes>
-                )}
-            </MyListProvider>
+                    {background && (
+                        <Routes>
+                            <Route path="movies/:id" element={<MovieInfo />} />
+                            <Route path="tv-shows/:id" element={<ShowInfo />} />
+                            <Route path="home/login" element={<Login />} />
+                            <Route path="home/register" element={<Register />} />
+                        </Routes>
+                    )}
+                </MyListProvider>
+            </AuthProvider>
         </div>
     );
 }
