7f73857b8ca3a8d49fad6a738d188ab18111ec43
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-27 22:44:11 -0300

    se agrega AuthContext para realizar las validaciones en firebase

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase-config";
+
+export const AuthContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("There is no auth provider");
+    return context;
+};
+
+export const AuthProvider = ({ children }) => {
+    const signup = (email, password) => {
+        createUserWithEmailAndPassword(auth, email, password);
+    };
+
+    const login = (email, password) => {
+        console.log(email, password);
+    };
+    return <AuthContext.Provider value={{ signup, login }}>{children}</AuthContext.Provider>;
+};
