6f63283ffe0399557103aa9d17b2006d1ee40bfa
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-29 00:30:08 -0300

    error de tipeo

--- src/routes/protectedRoute/ProtectedRoute.jsx
+++ src/routes/protectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ children }) {
-    const { user, loading } = useAuth;
+    const { user, loading } = useAuth();
 
     if (loading) return <h1>Loading</h1>;
 
