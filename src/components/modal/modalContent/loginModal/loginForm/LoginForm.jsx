81e185720362cd86bfc0b23950ebecf2994502db
Nicolas Cervino <nicolascervino2@gmail.com>
2022-12-30 20:22:48 -0300

    se agrega login con redes sociales

--- src/components/modal/modalContent/loginModal/loginForm/LoginForm.jsx
+++ src/components/modal/modalContent/loginModal/loginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { faGoogle, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const LoginForm = ({ submit, email, setEmail, password, setPassword }) => {
     const [hidePassword, setHidePassword] = useState(true);
@@ -10,48 +11,67 @@ const LoginForm = ({ submit, email, setEmail, password, setPassword }) => {
     };
 
     return (
-        <form className="row" onSubmit={submit}>
-            <div className="col-12">
-                <div className="form-outline">
-                    <input
-                        type="email"
-                        id="inputUsername"
-                        className="form-control form-control-sm"
-                        defaultValue={email}
-                        placeholder={"Ingrese un email registrado"}
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label className="form-label unselectable">Email</label>
-                </div>
-            </div>
-            <div className="col-12">
-                <div className="form-outline">
-                    <div className="d-flex align-items-center form-control form-control-sm">
+        <>
+            <form className="row" onSubmit={submit.email}>
+                <div className="col-12">
+                    <div className="form-outline">
                         <input
-                            type={hidePassword ? "password" : "text"}
-                            className="password-input"
-                            defaultValue={password}
-                            placeholder={"6 caracteres como minimo"}
-                            onChange={(e) => setPassword(e.target.value)}
-                            minLength={6}
+                            type="email"
+                            id="inputUsername"
+                            className="form-control form-control-sm"
+                            defaultValue={email}
+                            placeholder={"Ingrese un email registrado"}
                             required
+                            onChange={(e) => setEmail(e.target.value)}
                         />
-                        {hidePassword ? (
-                            <FontAwesomeIcon icon={faEye} onClick={() => setHidePassword(!hidePassword)} />
-                        ) : (
-                            <FontAwesomeIcon icon={faEyeSlash} onClick={() => setHidePassword(!hidePassword)} />
-                        )}
+                        <label className="form-label unselectable">Email</label>
                     </div>
-                    <label className="form-label unselectable">Password</label>
+                </div>
+                <div className="col-12">
+                    <div className="form-outline">
+                        <div className="d-flex align-items-center form-control form-control-sm">
+                            <input
+                                type={hidePassword ? "password" : "text"}
+                                className="password-input"
+                                defaultValue={password}
+                                placeholder={"6 caracteres como minimo"}
+                                onChange={(e) => setPassword(e.target.value)}
+                                minLength={6}
+                                required
+                            />
+                            {hidePassword ? (
+                                <FontAwesomeIcon icon={faEye} onClick={() => setHidePassword(!hidePassword)} />
+                            ) : (
+                                <FontAwesomeIcon icon={faEyeSlash} onClick={() => setHidePassword(!hidePassword)} />
+                            )}
+                        </div>
+                        <label className="form-label unselectable">Password</label>
+                    </div>
+                </div>
+                <div className="col-12">
+                    <button type="submit" className="login-button btn btn-sm px-4" disabled={validateButton()}>
+                        INICIAR SESIÓN
+                    </button>
+                </div>
+            </form>
+            <div className="row">
+                <hr className="my-3"></hr>
+                <div className="col-12 d-flex flex-column gap-2">
+                    <p className="m-0">Iniciar sesion con red social..</p>
+                    <div className="d-flex gap-3">
+                        <button onClick={submit.google} className="btn brand-login login--google">
+                            <FontAwesomeIcon icon={faGoogle} />
+                        </button>
+                        <button onClick={submit.facebook} className="btn brand-login login--facebook">
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </button>
+                        <button onClick={submit.twitter} className="btn brand-login login--twitter">
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div className="col-12">
-                <button type="submit" className="login-button btn btn-sm px-4" disabled={validateButton()}>
-                    INICIAR SESIÓN
-                </button>
-            </div>
-        </form>
+        </>
     );
 };
 
--- src/routes/login/Login.jsx
+++ src/routes/login/Login.jsx
@@ -1,3 +1,4 @@
+import { async } from "@firebase/util";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import CustomModal from "../../components/modal/CustomModal";
@@ -6,7 +7,7 @@ import { useAuth } from "../../context/AuthContext";
 
 function Login() {
     const [show, setShow] = useState(true);
-    const { login } = useAuth();
+    const { login, loginWithGoogle, loginWithFacebook, loginWithTwitter } = useAuth();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -26,6 +27,33 @@ function Login() {
         }
     };
 
+    const handleGoogleLogin = async () => {
+        try {
+            await loginWithGoogle();
+            navigate("/home");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleFacebookLogin = async () => {
+        try {
+            await loginWithFacebook();
+            navigate("/home");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const handleTwitterLogin = async () => {
+        try {
+            await loginWithTwitter();
+            navigate("/home");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const handleClose = () => {
         setShow(false);
         if (location.state !== null) {
@@ -39,7 +67,14 @@ function Login() {
         <CustomModal show={show} handleClose={handleClose} size={"md"}>
             <ModalContent
                 type={"login"}
-                login={{ email: email, setEmail: setEmail, password: password, setPassword: setPassword, submit: submit, errorCode: error }}
+                login={{
+                    email: email,
+                    setEmail: setEmail,
+                    password: password,
+                    setPassword: setPassword,
+                    submit: { email: submit, google: handleGoogleLogin, facebook: handleFacebookLogin, twitter: handleTwitterLogin },
+                    errorCode: error,
+                }}
                 handleClose={handleClose}
             />
         </CustomModal>
