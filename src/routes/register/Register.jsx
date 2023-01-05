077d337ad9a9984a54aa612672531538ae8987cb
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-05 20:01:25 -0300

    se almacena al usuario en la base de datos al registrarse con email y contraseña

--- src/routes/register/Register.jsx
+++ src/routes/register/Register.jsx
@@ -3,11 +3,13 @@ import { useNavigate } from "react-router-dom";
 import CustomModal from "../../components/modal/CustomModal";
 import RegisterModal from "../../components/modal/modalContent/registerModal/RegisterModal";
 import { useAuth } from "../../context/AuthContext";
+import { useDb } from "../../hooks/useDb";
 import useModal from "../../hooks/useModal";
 
 function Register() {
     const [show, handleClose] = useModal("/home");
     const { signup } = useAuth();
+    const [addUser] = useDb();
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -19,7 +21,9 @@ function Register() {
         e.preventDefault();
         setError("");
         try {
-            await signup(email, password);
+            signup(email, password).then((data) => {
+                addUser(data.user);
+            });
             navigate("/home");
         } catch (error) {
             setError(error.code);
