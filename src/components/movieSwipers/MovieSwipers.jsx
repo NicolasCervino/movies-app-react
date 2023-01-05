df8c61baa095ad2f4fd14f40ca3e7d13f7442bad
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-05 16:45:46 -0300

    se agrega un swiper con la lista del usuario

--- src/components/movieSwipers/MovieSwipers.jsx
+++ src/components/movieSwipers/MovieSwipers.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import MovieSwiper from "../swiper/MovieSwiper";
 import Api from "../../service/api";
+import { useAuth } from "../../context/AuthContext";
+import { useMyList } from "../../context/ListContext";
 
 const MovieSwipers = () => {
     const [popularMovies, setPopularMovies] = useState(null);
     const [topMovies, setTopMovies] = useState(null);
+    const { user } = useAuth();
+    const { moviesList } = useMyList();
 
     useEffect(() => {
         Api.getPopular("movie")
@@ -24,6 +28,13 @@ const MovieSwipers = () => {
 
     return (
         <div className="row px-2">
+            {user && moviesList.length > 0 && (
+                <div className="col-12 justify-content-center p-2">
+                    <h3 className="text-white text-uppercase">Peliculas en Mi lista:</h3>
+                    <MovieSwiper elements={moviesList} type="movie"></MovieSwiper>
+                </div>
+            )}
+
             <div className="col-12 justify-content-center p-2">
                 <h3 className="text-white text-uppercase">Peliculas populares:</h3>
                 <MovieSwiper elements={popularMovies} type="movie" category={"popular"}></MovieSwiper>
--- src/components/showSwipers/ShowSwipers.jsx
+++ src/components/showSwipers/ShowSwipers.jsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
 import MovieSwiper from "../swiper/MovieSwiper";
 import Api from "../../service/api";
+import { useAuth } from "../../context/AuthContext";
+import { useMyList } from "../../context/ListContext";
 
 const ShowSwipers = () => {
     const [shows, setShows] = useState(null);
     const [topShows, setTopShows] = useState(null);
+
+    const { user } = useAuth();
+    const { showsList } = useMyList();
 
     useEffect(() => {
         Api.getPopular("tv")
@@ -24,6 +29,12 @@ const ShowSwipers = () => {
 
     return (
         <div className="row px-2">
+            {user && showsList.length > 0 && (
+                <div className="col-12 justify-content-center p-2">
+                    <h3 className="text-white text-uppercase">Series en Mi lista:</h3>
+                    <MovieSwiper elements={showsList} type="tv"></MovieSwiper>
+                </div>
+            )}
             <div className="col-12 justify-content-center p-2">
                 <h3 className="text-white text-uppercase">Series Populares:</h3>
                 <MovieSwiper elements={shows} type="tv" category={"popular"}></MovieSwiper>
