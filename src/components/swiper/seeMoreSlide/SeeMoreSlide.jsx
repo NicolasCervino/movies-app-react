086071b93b11d5bf9c5bd1093f12c1d1c31e44d6
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-06 01:06:07 -0300

    se usa bigList

--- src/components/swiper/seeMoreSlide/SeeMoreSlide.jsx
+++ src/components/swiper/seeMoreSlide/SeeMoreSlide.jsx
@@ -3,14 +3,26 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 import "./see-more.css";
 
-const SeeMoreSlide = ({ type, category }) => {
+const SeeMoreSlide = ({ type, category, bigList }) => {
     return (
         <div className="see-more card ">
             <div className="card-body d-flex justify-content-center align-items-center flex-column">
-                <Link className="see-more-link" to={type === "movie" ? `/movies#${category}` : `/tv-shows#${category}`}>
-                    <FontAwesomeIcon icon={faAnglesRight} />
-                </Link>
-                <p>Ver mas...</p>
+                {category && (
+                    <>
+                        <Link className="see-more-link" to={type === "movie" ? `/movies#${category}` : `/tv-shows#${category}`}>
+                            <FontAwesomeIcon icon={faAnglesRight} />
+                        </Link>
+                        <p className="unselectable">Ver mas...</p>
+                    </>
+                )}
+                {bigList && (
+                    <>
+                        <Link className="see-more-link" to="/my-list">
+                            <FontAwesomeIcon icon={faAnglesRight} />
+                        </Link>
+                        <p className="unselectable">Ver mas...</p>
+                    </>
+                )}
             </div>
         </div>
     );
