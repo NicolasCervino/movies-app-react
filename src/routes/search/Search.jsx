4844f9b627af9f403833b6233cd4c58f33f55ca2
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-06 03:37:16 -0300

    se agrega CategorySelector

--- src/routes/search/Search.jsx
+++ src/routes/search/Search.jsx
@@ -2,25 +2,28 @@ import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import Api from "../../service/api";
 import InfiniteCardScroll from "../../components/InfiniteScroll/InfiniteCardScroll";
+import CategorySelector from "../home/categorySelector/CategorySelector";
 
 const Search = () => {
     const [searchParams] = useSearchParams();
     const [query, setQuery] = useState(searchParams.get("q"));
     const [elements, setElements] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
+    const [category, setCategory] = useState("movie");
 
     useEffect(() => {
         setQuery(searchParams.get("q"));
         if (query !== null) {
-            Api.getSearch("movie", query).then((data) => {
+            Api.getSearch(category, query).then((data) => {
                 setTotalPages(data.total_pages);
                 setElements(data.results);
             });
         }
-    }, [searchParams.get("q"), query]);
+    }, [searchParams.get("q"), query, category]);
 
     return (
         <div className="row">
+            <CategorySelector category={category} setCategory={setCategory} search={true} />
             {!query || elements.length === 0 ? (
                 <h1 className="text-white">No hay resultados...</h1>
             ) : (
@@ -33,7 +36,7 @@ const Search = () => {
                         <InfiniteCardScroll
                             elements={elements}
                             setElements={setElements}
-                            type={["movie", "search", query]}
+                            type={[category, "search", query]}
                             totalPages={totalPages}
                         />
                     </div>
