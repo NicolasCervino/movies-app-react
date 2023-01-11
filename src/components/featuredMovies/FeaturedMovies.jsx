1714ba5e40c112bc056d46de33282b98483e279a
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-11 16:32:52 -0300

    se utiliza useRandomPage

--- src/components/featuredMovies/FeaturedMovies.jsx
+++ src/components/featuredMovies/FeaturedMovies.jsx
@@ -2,20 +2,23 @@ import { useState, useEffect } from "react";
 import Api from "../../service/api";
 import LoadingCarrousel from "../loaders/loadingCarrousel/LoadingCarrousel";
 import MainCarrousel from "../carrousel/mainCarrousel/MainCarrousel";
+import useRandomPage from "../../hooks/useRandomPage";
 
 const FeaturedMovies = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(false);
+    const page = useRandomPage("popular", "movie");
 
     useEffect(() => {
         setLoading(true);
-        Api.getPopular("movie")
+        Api.getPopular("movie", page)
             .then((data) => {
-                setMovies(data.results);
+                const movies = data.results.filter((m) => m.backdrop_path !== null && m.overview !== "");
+                setMovies(movies);
             })
             .catch((error) => console.log(error))
             .finally(setTimeout(() => setLoading(false), 1000));
-    }, []);
+    }, [page]);
 
     return loading ? <LoadingCarrousel /> : <MainCarrousel movies={movies} type={"movie"} size={5} />;
 };
--- src/components/featuredShows/FeaturedShows.jsx
+++ src/components/featuredShows/FeaturedShows.jsx
@@ -2,21 +2,23 @@ import { useState, useEffect } from "react";
 import Api from "../../service/api";
 import LoadingCarrousel from "../loaders/loadingCarrousel/LoadingCarrousel";
 import MainCarrousel from "../carrousel/mainCarrousel/MainCarrousel";
+import useRandomPage from "../../hooks/useRandomPage";
 
 const FeaturedShows = () => {
     const [shows, setShows] = useState([]);
     const [loading, setLoading] = useState(false);
+    const page = useRandomPage("top_rated", "tv");
 
     useEffect(() => {
         setLoading(true);
-        Api.getTop("tv")
+        Api.getTop("tv", page)
             .then((data) => {
                 const shows = data.results.filter((s) => s.backdrop_path !== null && s.overview !== "");
                 setShows(shows);
             })
             .catch((error) => console.log(error))
             .finally(setTimeout(() => setLoading(false), 1000));
-    }, []);
+    }, [page]);
 
     return loading ? <LoadingCarrousel /> : <MainCarrousel movies={shows} type={"tv"} size={5} />;
 };
