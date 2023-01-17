81ae4e44529425944dc5ec469d2fb275115aa320
Nicolas Cervino <nicolascervino2@gmail.com>
2023-01-17 01:04:14 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/movies-app-react">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
